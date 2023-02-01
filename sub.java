96ed86ed56b9999104f9c4e4b653ac7e3958d967
Chandrakant <chandrakantmkamble1998@gmail.com>
2023-02-01 23:10:36 +0530

    git push -u origin main

--- sub.java
+++ sub.java
@@ -1,7 +1,7 @@
 public class Sum{
       public static void main (String args[]) {
-            int a = 5;
-            int b = 10;
+            int a = 50;
+            int b = 20;
             System.out.println("Substraction is as follows"+(a-b));
       }
 
